4dc3c28a5d6c4b71337a8fa6018ef700a6ed4adb
EL OUARRAT Youssef <youssef.el-ouarrat@etu.univ-amu.fr>
2021-01-22 17:32:32 +0100

    TD2

--- tests/Unit/RepositoryTest.php
+++ tests/Unit/RepositoryTest.php
@@ -20,22 +20,71 @@ class RepositoryTest extends TestCase
         $this->data = new Data();
         $this->repository = new Repository();
         $this->repository->createDatabase();
+        
     }
+
 
     function testTeamsAndInsertTeam(): void
     {
         $teams = $this->data->teams();
         $this->assertEquals($this->repository->insertTeam($teams[4]), 5);
-        $this->assertEquals($this->repository->teams(), [$teams[4]]);
+        $this->assertEquals($this->repository->insertTeam($teams[2]), 3);
+        $this->assertEquals($this->repository->insertTeam($teams[7]), 8);
+        $this->assertEquals($this->repository->teams(), [$teams[2], $teams[4], $teams[7]]);
     }
+    
 
     function testMatchesAndInsertMatch(): void
     {
         $teams = $this->data->teams();
         $matches = $this->data->matches();
-        // TODO : tester la méthode insertMatch
-        $this->assertEquals($this->repository->insertMatch($matches[4]), 5);
-        $this->assertEquals($this->repository->matches(), [$matches[4]]);
+        $this->assertEquals($this->repository->insertTeam($teams[6]), 7);
+        $this->assertEquals($this->repository->insertTeam($teams[18]), 19);
+        $this->assertEquals($this->repository->insertTeam($teams[5]), 6);
+        $this->assertEquals($this->repository->insertTeam($teams[10]), 11);
+        $this->assertEquals($this->repository->insertMatch($matches[5]), 6);
+        $this->assertEquals($this->repository->insertMatch($matches[0]), 1);
+        $this->assertEquals($this->repository->matches(), [$matches[0], $matches[5]]);
     }
-     
+
+    
+    function testfillDatabase(): void
+    {
+        $teams = $this->data->teams();
+        $matches = $this->data->matches();
+        
+        $this->repository->fillDatabase();
+
+        $this->assertEquals($this->repository->teams(),$teams );
+        $this->assertEquals($this->repository->matches(),$matches );
+    }
+
+    function testTeam(): void
+    {
+        $this->repository->fillDatabase();
+
+        foreach ($this->data->teams() as $team) 
+        {
+            $this->assertEquals($this->repository->team($team['id']), $team);
+        }
+    }
+
+    function testTeamThrowsExceptionIfTeamDoesNotExist(): void
+    {
+        $this->repository->fillDatabase();
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Équipe inconnue');
+        $this->repository->team(10000);
+    }
+
+    function testUpdateRanking(): void
+    {
+        $this->repository->updateRanking();
+        $this->repository->fillDatabase();
+        $this->repository->updateRanking();
+        $this->repository->updateRanking();
+        $ranking = DB::table('ranking')->orderBy('rank')->get()->toArray();
+        $this->assertEquals($ranking, $this->data->expectedSortedRanking());
+    }
+
 }
