eb32cce575fd9f9ca907d18255cde07c369c7f67
EL OUARRAT Youssef <youssef.el-ouarrat@etu.univ-amu.fr>
2021-02-04 09:49:35 +0100

    Nouvelle alimentation de la bd .tests passes ( td2)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,18 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         touch('database/database.sqlite');
-        $repository = new Repository(); 
+        $repository = new Repository();
         $repository->createDatabase();
-
-        // $repository->insertTeam(['name' => 'Marseille']);
-        // $repository->insertTeam(['name' => 'Paris']);
-
-        // $repository->insertMatch(['team0' =>2, 'team1' =>5, 
-        // 'score0'=>3, 'score1'=>0, 
-        // 'date'=>'2048-01-01 10:00']);
-        
-       
-        
+        $repository->fillDatabase();
+        $repository->updateRanking();
     }
 
 }
